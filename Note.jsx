5ee7b7d9e33949e11aad1ce8d23bb5a9fe0bbe94
sikha22 <84958102+sikha22@users.noreply.github.com>
2021-06-06 10:54:59 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note ">
+      <h1>Bootcamp Organised By ShapeAI</h1>
+      
+        <ul>
+        <li>JavaScript and React.js</li>
+        <li>Python and Machine Learning </li>
+        <li>python and computer Vision</li>
+        <li> Web Dev with Nodejs</li>
+      </ul>
+       
+    </div>
+  );
+}
+
+export default Note;
